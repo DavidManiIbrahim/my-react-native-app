070e7e9729bc4f1ab780924a26ae4387ace4607c
jiggy <olaojodominion@gmail.com>
2025-04-16 19:21:38 +0100

    firdt commit

---
+++ components/Test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { ThemedView } from './ThemedView'
+import { ThemedText } from './ThemedText'
+
+export default function test() {
+  return (
+   <ThemedView>
+    <ThemedText>
+        hello world
+    </ThemedText>
+   </ThemedView>
+  )
+}
